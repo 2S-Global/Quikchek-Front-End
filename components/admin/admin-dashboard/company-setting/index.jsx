aab18832d1c14f68ba4622b568e413a0b216250b
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-10 17:14:37 +0530

    added feature to show company name

--- components/admin/admin-dashboard/company-setting/index.jsx
+++ components/admin/admin-dashboard/company-setting/index.jsx
@@ -1,17 +1,21 @@
 import MobileMenu from "../../../header/MobileMenu";
 import DashboardHeader from "../../../header/DashboardAdminheader";
 import DashboardEmployerSidebar from "../../../header/DashboardAdminsidebar";
-
+import axios from "axios";
 import CopyrightFooter from "../../CopyrightFooter";
 
 import Companytable from "./components/table";
 
 import AddfieldModal from "./components/modals/addfield";
+import { useSearchParams } from "next/navigation";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const Index = () => {
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
   const [isModalOpen, setIsModalOpen] = useState(false);
-
+  const [companyName, setCompanyName] = useState("");
+  const apiurl = process.env.NEXT_PUBLIC_API_URL;
   const openModalRH = () => {
     setIsModalOpen(true);
     document.body.style.overflow = "hidden"; // Disable background scrolling
@@ -21,6 +25,32 @@ const Index = () => {
     setIsModalOpen(false);
     document.body.style.overflow = "auto"; // Re-enable background scrolling
   };
+
+  useEffect(() => {
+    const token = localStorage.getItem("Super_token");
+    if (!token) {
+      setError("Token not found. Please log in again.");
+      return;
+    }
+
+    const fetchCompanyName = async () => {
+      try {
+        const response = await axios.post(
+          `${apiurl}/api/auth/get_company_details`,
+          { company_id: id },
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        setCompanyName(response.data.data.name);
+      } catch (error) {
+        console.error("Error fetching company name:", error);
+      }
+    };
+
+    fetchCompanyName();
+  }, [apiurl]);
+
   return (
     <>
       <div className="page-wrapper dashboard ">
@@ -41,7 +71,7 @@ const Index = () => {
               <div className="col-lg-12">
                 <div className="applicants-widget ls-widget">
                   <div className="widget-title">
-                    <h4>Field List</h4>
+                    <h4>Field List ( {companyName})</h4>
 
                     <span
                       onClick={openModalRH}

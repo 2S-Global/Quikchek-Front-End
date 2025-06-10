72a4a9377566a9b7141a8f0c2d16a57a626250b5
2S-Global <2sglobaltechnologies@gmail.com>
2025-06-10 12:21:44 +0530

    refactor AuthWrapper to validate token and manage client state

--- app/(user)/AuthWrapper.jsx
+++ app/(user)/AuthWrapper.jsx
@@ -1,21 +1,40 @@
-// app/(user)/AuthWrapper.jsx
 "use client";
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import axios from "axios";
 
 export default function AuthWrapper({ children }) {
   const router = useRouter();
   const [isClient, setIsClient] = useState(false);
+  const apiurl = process.env.NEXT_PUBLIC_API_URL;
+
+  const validToken = async () => {
+    try {
+      const token = localStorage.getItem("Admin_token");
+      const response = await axios.get(`${apiurl}/api/auth/validtoken`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (response.data.success) {
+        setIsClient(true);
+      } else {
+        localStorage.removeItem("Admin_token");
+        router.push("/");
+      }
+    } catch (err) {
+      localStorage.removeItem("Admin_token");
+      router.push("/");
+    }
+  };
 
   useEffect(() => {
     const token = localStorage.getItem("Admin_token");
     if (!token) {
       router.push("/");
     } else {
-      setIsClient(true); // only render after client check
+      validToken();
     }
-  }, []);
+  }, [apiurl]);
 
   if (!isClient) return null;
 

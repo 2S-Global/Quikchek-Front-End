264200f17b3e3f9832da895710f4671e9b79cbe5
2S-Global <2sglobaltechnologies@gmail.com>
2025-09-02 11:42:36 +0530

    fix: Remove unused state variables in AddfieldModal and Companytable components

--- components/admin/admin-dashboard/company-setting/components/modals/addfield.jsx
+++ components/admin/admin-dashboard/company-setting/components/modals/addfield.jsx
@@ -17,6 +17,8 @@ const AddfieldModal = ({ show, onClose }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
+  const [errorId, setErrorId] = useState(null);
+  const [message_id, setMessage_id] = useState(null);
   const router = useRouter();
   const apiurl = process.env.NEXT_PUBLIC_API_URL;
 
@@ -52,11 +54,13 @@ const AddfieldModal = ({ show, onClose }) => {
       }
 
       setSuccess(response.data.message);
+      setMessage_id(Date.now());
       router.push(`/admin/company-setting?id=${id}`);
     } catch (err) {
       setError(
         err.response?.data?.message || "Registration failed. Try again."
       );
+      setErrorId(Date.now());
     } finally {
       setLoading(false);
     }
@@ -89,7 +93,12 @@ const AddfieldModal = ({ show, onClose }) => {
             <div className="modal-body">
               <form onSubmit={handleSubmit}>
                 {/* Response Message */}
-                <MessageComponent error={error} success={success} />
+                <MessageComponent
+                  error={error}
+                  success={success}
+                  errorId={errorId}
+                  message_id={message_id}
+                />
 
                 <div className="mb-3">
                   <label htmlFor="name" className="form-label">
